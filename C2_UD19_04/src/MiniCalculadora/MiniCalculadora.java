71a3d761db66c913e5d87d1c15d0806cb545611c
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-07-29 12:27:46 +0200

    added MiniCalculadora class

--- C2_UD19_04/src/MiniCalculadora/MiniCalculadora.java
+++ C2_UD19_04/src/MiniCalculadora/MiniCalculadora.java
@@ -1,5 +1,61 @@
 package MiniCalculadora;
 
 public class MiniCalculadora {
+	double result;
+
+	/**
+	 * Constructor
+	 */
+	public MiniCalculadora() {
+
+	}
+
+	/**
+	 * Add
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double sum(double a, double b) {
+		result = a + b;
+		return result;
+	}
+
+	/**
+	 * Substract
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double rest(double a, double b) {
+		result = a - b;
+		return result;
+	}
+
+	/**
+	 * Multiply
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double multiplication(double a, double b) {
+		result = a * b;
+		return result;
+	}
+
+	/**
+	 * Split
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double division(double a, double b) {
+		result = a / b;
+		return result;
+	}
 
 }
