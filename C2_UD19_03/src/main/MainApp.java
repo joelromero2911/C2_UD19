22922e05658e7e03742365d1be964428191b738d
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-07-29 11:11:42 +0200

    C2_UD19_03 v1.0

---
+++ C2_UD19_03/src/main/MainApp.java
@@ -0,0 +1,13 @@
+package main;
+
+import swing.AplicacionGrafica;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		AplicacionGrafica ventana = new AplicacionGrafica();
+
+	}
+
+}
