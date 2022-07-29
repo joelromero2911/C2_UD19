064b07ac9c62a9c5c34743854fe139e786505e06
Celestinvib <celestinvib@gmail.com>
2022-07-29 10:21:17 +0200

    C2_UD19_01 V1.0

--- C2_UD19_01/src/AplicacionGrafica/AplicacionGrafica.java
+++ C2_UD19_01/src/AplicacionGrafica/AplicacionGrafica.java
@@ -1,60 +1,78 @@
 package AplicacionGrafica;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class AplicacionGrafica extends JFrame {
-
+	
+	/*Attribute*/
+	
 	private JPanel contentPane;
+	
+	
+	/*Constructor*/
 	
 	public AplicacionGrafica() {
 		
-		setTitle("Saludador");
+		/*Definition of the window */
 		
-		// Coordenadas X Y de la aplicacion y su altura ylongitud
+		
+		setTitle("Saludador");//PopUp window title
+		
+		/* X Y coordinates of the application and its height and length */
 		setBounds(700, 200, 500, 500);
 		
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);//Close the window  when the operation is close
 		
-		setVisible(true);
+		setVisible(true);//It allows the window to be visible
 		
+		/*Creation and added the panel to the window*/
 		contentPane = new JPanel();
 		
 		contentPane.setLayout(null);
 		
 		setContentPane(contentPane);
 		
+		
+		/*Creation, definition of dimensions and added to the panel of a label*/
 		JLabel etiqueta = new JLabel("Escribe un nombre para saludar");
 		
 		etiqueta.setBounds(155, 20, 200, 50);
-		
+	    
 		contentPane.add(etiqueta);
 		
-		
+		/*Creation of a border, it'll serve to be able to see the next elements of the panel without putting the mouse over them*/ 
+		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+
+		/*Creation, definition of dimensions and added to the panel of a textfield*/	
 		JTextField textField = new JTextField();
 		
 		textField.setBounds(100, 110, 300, 30);
 		
 		contentPane.add(textField);
 		
+		textField.setBorder(border); 
 		
+		/*Creation, definition of dimensions and added to the panel of a button*/			
 		JButton btn = new JButton("¡Saludar!");
 		
 		btn.setBounds(200, 190, 89,23);
 		
 		contentPane.add(btn);
 		
+		btn.setBorder(border);
+
+	    /*Creation of a Click Listener*/	
 		ActionListener al = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) { //The action that will be performed when the listener is triggered
 				JOptionPane.showMessageDialog(null,"¡Hola "+textField.getText()+"!");
 				
 			}	
 		};
 		
-		btn.addActionListener(al);
-		
+		btn.addActionListener(al); //Assign of the listener to the button element		
 	}
-	
-	
 }
